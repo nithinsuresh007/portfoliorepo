1ac3cbf85e3160ca0da1d592679e13a07025a74c
nithinsuresh007 <nithinsuresh7025@gmail.com>
2024-02-04 20:27:46 +0530

    added tailwind

---
+++ tailwindlearning/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <section>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo reprehenderit perspiciatis fugiat iusto ducimus, deleniti ab iure magni saepe explicabo.
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default App
